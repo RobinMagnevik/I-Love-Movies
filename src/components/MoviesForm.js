e9d02c1d4634680f10b2cf0c5dca436d3424b7f1
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-05-15 17:41:15 +0200

    Alex & Daniel made Redux Form with title & characters input with simple validation field required

---
+++ src/components/MoviesForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+const validate = v => {
+  const errors = {}
+  if (!v.movieTitle) {
+    errors.movieTitle = '(Required)'
+  } 
+  if (!v.mainCharacter) {
+    errors.mainCharacter = '(Required)'
+  } 
+  return errors
+}
+
+const fields = ({input, label, type, meta: { touched, error }}) => (
+  <div>
+    <label>{label}</label>{touched && (error && <span className="errorMessage">{error}</span>) }
+    <div>
+    <input {...input} placeholder={label} type={type} />  
+    </div>
+  </div>
+)
+
+const MoviesForm = ({handleSubmit, valid}) => (
+    <form onSubmit={handleSubmit}>
+      <Field name="movieTitle"  type="text" component={fields} label="Movie title" />
+      <Field name="mainCharacter" type="text" component={fields} label="Main characrer(s)" />
+      <div>
+        <button type="submit" disabled={!valid}>Submit</button>
+      </div>
+    </form>
+  )
+
+
+export default reduxForm({
+  form: 'moviesFormValidation', 
+  validate, 
+})(MoviesForm)

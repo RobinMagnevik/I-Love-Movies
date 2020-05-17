7a0160e4fce3e3ffe7a698610d837dcf71faa798
Daniel Johannesson <daniel@johannesson.cz>
2020-05-18 00:59:23 +0200

    init new component

---
+++ src/components/AddTitles.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MoviesForm from './MoviesForm'
+
+const AddTitles = () => {
+	return (
+		<div>
+			<div>
+				<MoviesForm/>
+			</div>
+		</div>
+	)
+}
+
+export default AddTitles

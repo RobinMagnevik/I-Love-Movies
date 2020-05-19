4c606c487e931e1705cba5c3988698b0155a635d
JohannaForsberg93 <johanna_forsberg@msn.com>
2020-05-19 16:17:55 +0200

    Fixat lite css och sånt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import MoviesForm from './components/MoviesForm';
+import React from "react";
+import "./App.css";
+import MoviesForm from "./components/MoviesForm";
+import Favorites from "./components/Favorites";
 
 function App() {
-  return (
-    <div className="App">
-	  <MoviesForm />
-    </div>
-  );
+	return (
+		<div className="App">
+			<MoviesForm />
+			<Favorites />
+		</div>
+	);
 }
 
 export default App;

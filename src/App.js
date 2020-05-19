d721b44410f9c0ad5380a7564df313f1a72dd8cb
Daniel Johannesson <daniel@johannesson.cz>
2020-05-19 23:16:47 +0200

    la header i app komponent istäälet för egen komp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AddTitles from './components/AddTitles'
-import Header from './components/header/Header'
+// import Header from './components/header/Header'
 import MoviePopUp from './components/MoviePopUp';
 import Favorites from "./components/Favorites";
+
+const [toShow, setToShow] = useState('')
 
 function App() {
 	return (
 		<div className="App">
-			<Header/>
-			<div className="add-title-page">
+			<header className="header-section">
+				<h1>I love movies</h1>
+				<nav className="nav-field">
+					<button className="nav-button" onClick={() => setToShow('addTitles')}>
+						Start
+					</button>
+					<button className="nav-button" onClick={() => setToShow('favorites')}>
+						Favoriter
+					</button>
+					<button className="nav-button" onClick={() => setToShow('inspiration')}>
+						Inspiration
+					</button>
+				</nav>
+			</header>
+			{/* <Header/> */}
+			<div className="add-title-page" style={toShow === 'addTitles' ? {display: 'block'} : {display: 'none'}}>
 			<AddTitles />
 			</div>
 			<div>
 			<MoviePopUp />
 			</div>
-			<div>
+			<div style={toShow === 'favorites' ? {display: 'block'} : {display: 'none'}}>
 			<Favorites />
+			</div>
+			<div style={toShow === 'inspiration' ? {display: 'block'} : {display: 'none'}}> 
+				{/* Place inspiration-page-tag here! :) */}
 			</div>
 		</div>
 	);
-	}
+}
 export default App;

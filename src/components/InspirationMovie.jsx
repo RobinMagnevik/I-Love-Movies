683543017b51598ae67be1cfd1dc06dd89ae6cee
Robin Magnevik <robin.mangevik@gmail.com>
2020-05-20 22:10:52 +0200

    added alot of movies and series as suggestions

--- src/components/InspirationMovie.jsx
+++ src/components/InspirationMovie.jsx
@@ -47,9 +47,29 @@ const Inspiration = () => {
     )
 }
 
-let movieList = ['The Godfather', 'The Shawshank Redemption', 'The Godfather: Part II', 'The Dark Knight', '12 Angry Men', "Schindler's List", 'The Lord of the Rings: The Return of the King', 'Pulp Fiction', 'The Lord of the Rings: The Fellowship of the Ring', 'Fight Club', 'Forrest Gump', 'Inception', 'Star Wars: Episode V - The Empire Strikes Back', 'The Lord of the Rings: The Two Towers', 'The Matrix', 'Goodfellas', 'Se7en'];
+let movieList = ['The Godfather', 'The Shawshank Redemption', 'The Godfather: Part II',
+'The Dark Knight', '12 Angry Men', "Schindler's List", 'The Lord of the Rings: The Return of the King',
+'Pulp Fiction', 'The Lord of the Rings: The Fellowship of the Ring', 'Fight Club', 'Forrest Gump',
+'Inception', 'Star Wars: Episode V - The Empire Strikes Back', 'The Lord of the Rings: The Two Towers',
+'The Matrix', 'Goodfellas', 'Se7en', 'Star Wars', 'The Green Mile', 'Interstellar', 'The Usual Suspects', 
+'The Lion King', 'The Pianist', 'Back to the Future', 'Psycho', 'Gladiator', 'The Departed', 'Casablanca', 
+'Joker', 'Django Unchained', 'The Shining', 'WALL-E', 'Avengers: Endgame', 'Aliens', 'Coco', 'Braveheart', 
+'Toy Story', 'Inglourious Basterds', 'Good Will Hunting', 'Full Metal Jacket', 'A Clockwork Orange', 
+'Snatch', 'Scarface', 'Taxi Driver', 'Indiana Jones and the Last Crusade', 'Heat', 'Die Hard', 
+'Batman Begins', 'Casino', 'The Wolf of Wall Street', 'Lock, Stock and Two Smoking Barrels', 
+'V for Vandetta', 'Warrior', 'Shutter Island', 'Jurassic Park', 'Finding Nemo', 'Kill Bill: Vol.1', 
+'The Big Lebowski', 'Catch Me If You Can', 'Logan', 'Hotel Rwanda', 'Rocky', 'Monsters, Inc.', 
+'The Terminator', 'Aladdin'];
 
-let serieList = ['Game of Thrones', 'Vikings', 'Breaking Bad', 'Band of Brothers', 'Chernobyl', 'The Wire', 'Rick and Morty', 'Sherlock', 'True Detective', 'Death Note', 'Fargo', 'The Office', 'Friends', 'Seinfeld']
+let serieList = ['Game of Thrones', 'Vikings', 'Breaking Bad', 'Band of Brothers', 'Chernobyl', 
+'The Wire', 'Rick and Morty', 'Sherlock', 'True Detective', 'Death Note', 'Fargo', 'The Office', 
+'Friends', 'Seinfeld', 'Hunter x Hunter', 'Black Mirror', 'Narcos', 'Peaky Blinders', 'Stranger Things', 
+'Shingeki no kyojin', 'Arrested Development', 'Rome', "It's Always Sunny In Philadelphia", 'Oz', 
+'House of Cards', 'Dragon Ball Z', 'Better Call Saul', 'Westworld', 'BoJack Horseman', 'Six Feet Under', 
+'South Park', 'The Simpsons', 'Top Gear', 'Downtown Abbey', 'Deadwood', 'The X Files', 'Archer', 
+'Mad Men', 'Dexter', 'Daredevil', 'Mindhunter', 'Making a Murderer', 'The Newsroom', 'Sacred Games', 
+'Skam', 'Shameless', 'Boardwalk Empire', 'Doctor Who', 'Dragon Ball', 'Twin Peaks', 'The Night Of', 
+'Big Little Lies', 'Mr.Robot', 'Silicon Valley', 'Hannibal', 'The Punisher', 'Community', 'Luther']
 
 async function fetchMovie(dispatch) {
     let randomNumber = Math.floor((Math.random() * movieList.length -1) + 1);

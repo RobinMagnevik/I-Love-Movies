5450771855e36cb4c21de5670a162b6fa5d70a18
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-05-21 18:07:49 +0200

    added startpage + css

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import MoviePopUp from "./MoviePopUp";
+import './startPage.css';
 
 const StartPage = () => {
-	return (
-		<div>
-			
+  const [toggle, setToggle] = useState(true);
+	const [todos, setTodos] = useState("")
+
+	useEffect(() => {
+		localStorage.setItem("hej", JSON.stringify(todos))
+	}, (todos))
+
+
+	function addTodo(todo) {
+		setTodos([todo, ...todos])
+	}
+
+
+  return (
+    <div>
+      {toggle && (
+        <div className="startPageDiv">
+		<div className="startPageInnerDiv">
+		<p className="startPageWelcomePhrase">Welcome, tell us who you are!</p>
+	
+		<input className="startPageInput" todos={todos} type="text"/><br/>
+		<button type="submit"
+            className="startPageWelcomeButton"
+            onClick={() => {setToggle((toggle) => !toggle); addTodo()}}
+          >Welcome!</button>
+
 		</div>
-	)
-}
+        </div>
+      )}
+	  {!toggle && (
+	  <MoviePopUp />
+	  )}
+    </div>
+  );
+};
 
 export default StartPage;

1ac09fb00de4608ce3009b921d73296100612473
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-05-21 15:11:12 +0200

    added to rootreducer

--- src/features/rootReducer.js
+++ src/features/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { reducer as addFavoriteListReducer } from './addFavoriteList'
 import { reducer as InspoMovieReducer } from './InspoMovie'
 
 const rootReducer = combineReducers({
-	inspoMovie: InspoMovieReducer
+	form: formReducer,
+	addFavoriteList: addFavoriteListReducer,
+  inspoMovie: InspoMovieReducer
 })
 
 export { rootReducer } 

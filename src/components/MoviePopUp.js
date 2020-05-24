417c69e4c4bfd69320db74026784efdeeb8d67bb
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-05-24 17:02:11 +0200

    amended popup effect

--- src/components/MoviePopUp.js
+++ src/components/MoviePopUp.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 import "../cssFolder/moviePopUp.css";
+import MoviePopUpEffect from './MoviePopUpEffect'
 
 const MoviePopUp = ({ item }) => {
-  const [toggle, setToggle] = useState(false);
+const [show, setShow] = useState(false);
 
   return (
-    <div> {toggle &&
-      <div
-        className="popUpModal"
-        onClick={() => setToggle((toggle) => !toggle)}
-      >
+	  <div>
+    <MoviePopUpEffect show={show}>
+      <div className="popUpModal" onClick={() => setShow(show => !show)}>
         <div className="popUpDiv">
-		<p className="popUpX" onClick={() => setToggle((toggle) => toggle)}>x</p>
+		<p className="popUpX" onClick={() => setShow(show => show)}> x </p>
           <div className="popUpImg"></div>
-          <p className="popUpTitle"><span>{item.film.title}</span></p>
+          <p className="popUpTitle"><span> {item.film.title} </span></p>
 		  <p className="popUpAbout">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel orci iaculis odio pellentesque aliquam. Nam consectetur posuere odio, sit amet finibus leo pharetra ac.</p>
           <span className="popUpGenre">Action</span>
           <span className="popUpYear">2012</span>
 		  <span className="popUpRating">Rating: 5</span>
         </div>
-      </div> }
+      </div> 
 	  <br/>
-	  <span className="popUpShowDetails" onClick={() => setToggle((toggle) => !toggle)}>Show details</span>
-    </div>
+	</MoviePopUpEffect> 
+	<p className="popUpShowDetails" onClick={() => setShow(show => !show)}> 
+		  {/* {show ? "hide" : "show"} */}
+		  Show details
+		</p>
+		</div>
   );
 };
 
@@ -30,45 +33,34 @@ export default MoviePopUp;
 
 
 // import React, { useState } from "react";
-// import { useSpring, animated } from 'react-spring';
-// import './MoviePopUp.css';
+// import "../cssFolder/moviePopUp.css";
 
-// const MoviePopUp = ({ movie }) => {
-// const [toggle, setToggle] = useState(false);
-//   const [fading, setFading] = useState(false);
-
-// //   const fade = useSpring({
-// // 	  opacity: fading ? 1 : 0
-// //   })
-
-// console.log("hej", movie)
+// const MoviePopUp = ({ item }) => {
+//   const [toggle, setToggle] = useState(false);
 
 //   return (
-//     <div> 
-
-//       <div 
+//     <div> {toggle &&
+//       <div
 //         className="popUpModal"
-		
+//         onClick={() => setToggle((toggle) => !toggle)}
 //       >
 //         <div className="popUpDiv">
-// 		{/* <p className="popUpX" onClick={() => {setFading(!fading); setToggle(!toggle)}}>x</p> */}
+// 		<p className="popUpX" onClick={() => setToggle((toggle) => toggle)}>x</p>
 //           <div className="popUpImg"></div>
-//           <p className="popUpTitle"><span> {movie.title} </span></p>
+//           <p className="popUpTitle"><span>{item.film.title}</span></p>
 // 		  <p className="popUpAbout">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel orci iaculis odio pellentesque aliquam. Nam consectetur posuere odio, sit amet finibus leo pharetra ac.</p>
 //           <span className="popUpGenre">Action</span>
 //           <span className="popUpYear">2012</span>
 // 		  <span className="popUpRating">Rating: 5</span>
 //         </div>
-//       </div> 
-	  
+//       </div> }
 // 	  <br/>
-// 	  <span className="popUpShowDetails" >Show details</span>
-//     </div> 
+// 	  <span className="popUpShowDetails" onClick={() => setToggle((toggle) => !toggle)}>Show details</span>
+//     </div>
 //   );
 // };
 
 // export default MoviePopUp;
-
 
 
 

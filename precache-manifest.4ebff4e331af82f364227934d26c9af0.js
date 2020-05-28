713735d8882eb4a6b5e9265146a7db5a49e3305b
Daniel Johannesson <daniel@johannesson.cz>
2020-05-28 11:59:26 +0200

    Updates

---
+++ precache-manifest.4ebff4e331af82f364227934d26c9af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed85b2085048d41a08fbf36b33b24b9",
+    "url": "/I-Love-Movies/index.html"
+  },
+  {
+    "revision": "31f9c2133019cab94922",
+    "url": "/I-Love-Movies/static/css/main.931fbe9b.chunk.css"
+  },
+  {
+    "revision": "69b2292d9e888f7d7080",
+    "url": "/I-Love-Movies/static/js/2.c1fe1f7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/I-Love-Movies/static/js/2.c1fe1f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f9c2133019cab94922",
+    "url": "/I-Love-Movies/static/js/main.e6a62d74.chunk.js"
+  },
+  {
+    "revision": "4d07294f033da66ce37c",
+    "url": "/I-Love-Movies/static/js/runtime-main.d2cf97cc.js"
+  }
+]);
